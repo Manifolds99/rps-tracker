1c33cd7a8e73eee87f6af202f5389eacd9df93fe
Manifolds99 <122555271+Manifolds99@users.noreply.github.com>
2023-01-31 11:51:15 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,36 @@
-input.onGesture(Gesture.Shake, function () {
-    OLED.init(128, 64)
-    Ties = 0
-    Rounds = 0
-    PB = 0
-    PA = 0
-    OLED.writeStringNewLine("Want to play a game?")
-    basic.pause(2000)
+input.onButtonPressed(Button.A, function () {
+    PA += 1
+    Rounds += 1
+    basic.showString("A")
     Update_Score()
+})
+input.onButtonPressed(Button.AB, function () {
+    Ties += 1
+    Rounds += 1
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    Update_Score()
+})
+input.onButtonPressed(Button.B, function () {
+    PB += 1
+    Rounds += 1
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . # .
+        # # # . .
+        `)
+    Update_Score()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    Reset()
 })
 function Update_Score () {
     OLED.clear()
@@ -14,19 +38,22 @@ function Update_Score () {
     OLED.newLine()
     OLED.writeStringNewLine("Player B:" + PB)
     OLED.newLine()
-    OLED.writeStringNewLine("Rounds:" + Rounds)
-    OLED.newLine()
     OLED.writeStringNewLine("Ties:" + Ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds:" + Rounds)
 }
-let PA = 0
+function Reset () {
+    OLED.init(128, 64)
+    Ties = 0
+    Rounds = 0
+    PB = 0
+    PA = 0
+    OLED.writeStringNewLine("Let's play a game!")
+    basic.pause(2000)
+    Update_Score()
+}
 let PB = 0
-let Rounds = 0
 let Ties = 0
-OLED.init(128, 64)
-Ties = 0
-Rounds = 0
-PB = 0
-PA = 0
-OLED.writeStringNewLine("Want to play a game?")
-basic.pause(2000)
-Update_Score()
+let Rounds = 0
+let PA = 0
+Reset()
